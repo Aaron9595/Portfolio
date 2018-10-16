daa3777e84a8639f5da10906d33a69340783a13a
Aaron <aaron.nelson1995@gmail.com>
2018-10-16 12:58:14 -0600

    Added main.js and assets folder

---
+++ main.js
@@ -0,0 +1,9 @@
+
+import { films } from '/assets/films.js'
+import { people } from '/assets/people.js'
+import { planets } from '/assets/planets.js'
+import { starships } from '/assets/starships.js'
+import { vehicles } from '/assets/vehicles.js'
+import { species } from '/assets/species.js'
+
+console.log(people)
